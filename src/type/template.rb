a35e450180fcc11d14da82f37b73295058c3dbc2
Scott Gerike <gerisc01@luther.edu>
2023-01-07 23:02:42 -0600

    Fix errors due to badly initialized template

--- src/type/template.rb
+++ src/type/template.rb
@@ -1,18 +1,11 @@
-require 'securerandom'
-require 'json'
-# require_relative '../db/collection_db'
 require_relative '../schema/schema'
-require_relative './list'
-# require_relative './template'
-require_relative '../exceptions'
 require_relative '../generator/type_generator'
-require_relative '../generator/db_generator'
 
-class Collection
+class Template
 
   @@schema = Schema.new
-  @@schema.key = "collection"
-  @@schema.display_name = "Collection"
+  @@schema.key = "template"
+  @@schema.display_name = "Template"
   @@schema.fields = {
     "id" => {:required => true, :type => String, :display_name => 'Id'},
     "key" => {:required => false, :type => String, :display_name => 'Key'},
