8b5e301a2f5b9e2e434aed6fec21121ee55f34ef
Scott Gerike <scott.gerike@gmail.com>
2024-07-10 22:16:52 -0500

    Adjust set field to work with template fields

--- src/actions/set_field.rb
+++ src/actions/set_field.rb
@@ -4,7 +4,7 @@ def set_field(id, key, value)
   begin
     item = Item.get(id)
     raise "item id '#{id}' not found" if item.nil?
-    item.public_send("#{key}=", value)
+    item.json[key] = value
     item.save!
   rescue Exception => e
     raise ListError::BadRequest, "Failed to set field on item: #{e.message}"
