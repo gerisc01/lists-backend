df35799b1ce358374e8e874c165179d0804f54af
Scott Gerike <scott.gerike@gmail.com>
2025-12-11 23:50:06 -0600

    Add dates api and associated cache to easier retrieve date info

--- src/api/dates_api.rb
+++ src/api/dates_api.rb
@@ -17,6 +17,136 @@ class Api < Sinatra::Base
     body result.to_json
   end
 
+  # Get the items from a range of dates for a given list
+  get '/api/dates/:list/items' do
+    raise ListError::BadRequest, "Path must contain a list id." if params['list'].to_s.empty?
+    raise ListError::BadRequest, "Query parameters must contain 'start' and 'end' dates." if params['start'].to_s.empty? || params['end'].to_s.empty?
+
+    start_date = Date.parse(params['start'])
+    end_date = Date.parse(params['end'])
+    raise ListError::BadRequest, "'start' date must be before 'end' date." if start_date > end_date
+
+    result = {}
+    (start_date..end_date).each do |date|
+      day = Day.get(date.to_s)
+      if !day.nil?
+        list = day.items.find { |d| d.id == params['list'] }
+        result[date.to_s] = list.items if !list.nil?
+      end
+    end
+
+    status 200
+    body result.to_json
+  end
+
+  # Add an item to a given day and list
+  post '/api/dates/:day/items' do
+    raise ListError::BadRequest, "Path must contain a date." if params['day'].to_s.empty?
+    day = Day.get(params['day'])
+    if day.nil?
+      day = Day.new({
+        'id' => params['day'],
+      })
+    end
+
+    json = JSON.parse(request.body.read)
+    raise ListError::BadRequest, "Request body must contain 'list' and 'item'." if !json.is_a?(Hash) || json['list'].to_s.empty? || json['item'].to_s.empty?
+    raise ListkError::NotFound, "List id '#{json['list']}' cannot be found" unless List.exist?(json['list'])
+
+    day.items = [] if day.items.nil?
+    daily_item = day.items.find { |d| d.id == json['list'] }
+    if daily_item.nil?
+      daily_item = DailyItem.new({
+        'id' => json['list'],
+        'items' => [json['item']]
+      })
+      day.add_item(daily_item)
+    else
+      daily_item.add_item(json['item'])
+    end
+    day.save!
+    Day.add_day_for_item(json['item'], day.id)
+
+    status 200
+    body day.to_schema_object.to_json
+  end
+
+  # Add a priority item to a given day and list
+  post '/api/dates/:day/priority' do
+    raise ListError::BadRequest, "Path must contain a date." if params['day'].to_s.empty?
+    day = Day.get(params['day'])
+    if day.nil?
+      day = Day.new({
+        'id' => params['day'],
+      })
+    end
+
+    json = JSON.parse(request.body.read)
+    raise ListError::BadRequest, "Request body must contain 'list' and 'item'." if !json.is_a?(Hash) || json['list'].to_s.empty? || json['item'].to_s.empty?
+    raise ListkError::NotFound, "List id '#{json['list']}' cannot be found" unless List.exist?(json['list'])
+
+    day.priorities = [] if day.priorities.nil?
+    daily_item = day.priorities.find { |d| d.id == json['list'] }
+    if daily_item.nil?
+      daily_item = DailyItem.new({
+        'id' => json['list'],
+        'items' => [json['item']]
+      })
+      day.add_prioritie(daily_item)
+    else
+      daily_item.add_item(json['item'])
+    end
+    day.save!
+
+    status 200
+    body day.to_schema_object.to_json
+  end
+
+  # Remove an item from a given day and list
+  delete '/api/dates/:day/items' do
+    raise ListError::BadRequest, "Path must contain a date." if params['day'].to_s.empty?
+    day = Day.get(params['day'])
+    raise ListError::NotFound, "Day '#{params['day']}' cannot be found" if day.nil?
+
+    json = JSON.parse(request.body.read)
+    raise ListError::BadRequest, "Request body must contain 'list' and 'item'." if !json.is_a?(Hash) || json['list'].to_s.empty? || json['item'].to_s.empty?
+    raise ListError::NotFound, "List id '#{json['list']}' cannot be found" unless List.exist?(json['list'])
+
+    daily_item = day.items.find { |d| d.id == json['list'] }
+    if !daily_item.nil?
+      daily_item.remove_item(json['item'])
+      # Update the reference in the day
+      day.items.map! { |d| d.id == json['list'] ? daily_item : d }
+    end
+    day.save!
+    Day.remove_day_for_item(json['item'], day.id)
+
+    status 200
+    body day.to_schema_object.to_json
+  end
+
+  # Remove a priority item from a given day and list
+  delete '/api/dates/:day/priorities' do
+    raise ListError::BadRequest, "Path must contain a date." if params['day'].to_s.empty?
+    day = Day.get(params['day'])
+    raise ListError::NotFound, "Day '#{params['day']}' cannot be found" if day.nil?
+
+    json = JSON.parse(request.body.read)
+    raise ListError::BadRequest, "Request body must contain 'list' and 'item'." if !json.is_a?(Hash) || json['list'].to_s.empty? || json['item'].to_s.empty?
+    raise ListError::NotFound, "List id '#{json['list']}' cannot be found" unless List.exist?(json['list'])
+
+    daily_item = day.priorities.find { |d| d.id == json['list'] }
+    if !daily_item.nil?
+      daily_item.remove_prioritie(json['item'])
+      # Update the reference in the day
+      day.priorities.map! { |d| d.id == json['list'] ? daily_item : d }
+    end
+    day.save!
+
+    status 200
+    body day.to_schema_object.to_json
+  end
+
   # Update the priority items for a given day and list
   put '/api/dates/:day/:list/priorities' do
     raise ListError::BadRequest, "Path must contain both a date and a list id." if params['day'].to_s.empty? || params['list'].to_s.empty?
@@ -51,6 +181,22 @@ class Api < Sinatra::Base
     body day.to_schema_object.to_json
   end
 
-  generate_schema_crud_methods 'dates', Day
+  #############################################################################
+  #                     ITEM SPECIFIC DATE ENDPOINTS                          #
+  #############################################################################
+  # Get all dates for a specific item
+  get '/api/items/:item/dates' do
+    raise ListError::BadRequest, "Path must contain an item id." if params['item'].to_s.empty?
+    dates = Day.get_days_for_item(params['item'])
+
+    status 200
+    body dates.to_json
+  end
+
+  #############################################################################
+  #                     GENERATE SCHEMA CRUD METHODS                          #
+  #############################################################################
+  generate_schema_endpoint :list, 'dates', Day
+  generate_schema_endpoint :get, 'dates', Day
 
 end
--- src/type/day.rb
+++ src/type/day.rb
@@ -3,6 +3,7 @@ require 'ruby-schema-storage'
 
 require_relative '../exceptions'
 require_relative '../storage'
+require 'pstore'
 
 class DailyItem
 
@@ -42,4 +43,57 @@ class Day
     end
   end
 
+  def self.build_full_day_index
+    Dir.mkdir('cache') unless Dir.exist?('cache')
+    item_to_days = PStore.new('cache/item_to_days.pstore')
+    item_to_days.transaction do
+      days = self.list
+      days.each do |day|
+        date = day.id
+        day.items.each do |daily_items|
+          daily_items.items.each do |item_id|
+            item_to_days[item_id] = [] unless item_to_days.key?(item_id)
+            item_to_days[item_id] << date
+          end
+        end
+      end
+    end
+  end
+
+  def self.get_days_for_item(item_id)
+    item_to_days = PStore.new('cache/item_to_days.pstore')
+    days = []
+    item_to_days.transaction(true) do
+      if item_to_days.key?(item_id)
+        days = item_to_days[item_id]
+      end
+    end
+    return days
+  end
+
+  def self.add_day_for_item(item_id, date)
+    item_to_days = PStore.new('cache/item_to_days.pstore')
+    item_to_days.transaction do
+      item_to_days[item_id] = [] unless item_to_days.key?(item_id)
+      item_to_days[item_id] << date unless item_to_days[item_id].include?(date)
+    end
+  end
+
+  def self.remove_day_for_item(item_id, date)
+    item_to_days = PStore.new('cache/item_to_days.pstore')
+    item_to_days.transaction do
+      if item_to_days.key?(item_id)
+        item_to_days[item_id].delete(date)
+        if item_to_days[item_id].empty?
+          item_to_days.delete(item_id)
+        end
+      end
+    end
+  end
+
+  def self.clear_day_index
+    if File.exist?('cache/item_to_days.pstore')
+      File.delete('cache/item_to_days.pstore')
+    end
+  end
 end
